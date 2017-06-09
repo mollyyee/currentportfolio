aeec6ff735e51d2fb507ba50a41c2f45f7fb5b2d
Molly Yee <molly.yee@berkeley.edu>
2017-06-09 00:54:52 -0700

    Add files via upload

--- js/webjavascript.js
+++ js/webjavascript.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
 	$(".fadein").delay(300).animate({opacity: 1 }, 1200);
+
+
+  $('#aboutone.aboutsubtitle').click(function(e){    
+  $('#two, #three').fadeOut(10, function(){
+      $('#one').fadeIn('slow');
+  });
 });
+  $('#abouttwo.aboutsubtitle').click(function(e){    
+  $('#one, #three').fadeOut(10, function(){
+      $('#two').fadeIn('slow');
+  });
+});
+  $('#aboutthree.aboutsubtitle').click(function(e){    
+  $('#one, #two').fadeOut(10, function(){
+      $('#three').fadeIn('slow');
+  });
+});
+
+$('#aboutone.aboutsubtitle').on('click', function(e){
+  e.preventDefault();
+  $(this).css('border-color', '#e29589', 
+  'border-bottom-width', '2px',
+  'border-bottom-style', 'solid',
+    );
+});
+
+});
+
+
+
+    // $('#btn-bk').click(function(e){    
+    //     $('#bank, #btn-bk').fadeOut('slow', function(){
+    //         $('#fancy, #btn').fadeIn('slow');
+    //     });
+    // });
 
 
 $(function() {
@@ -17,3 +51,4 @@ $(function() {
     }
   });
 });
+
